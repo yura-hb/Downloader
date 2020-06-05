66081b3c6a5015eaf938e1a74ab0033ddcaf5e57
yura_hb <ggaev.work@gmail.com>
2020-06-05 17:53:04 +0200

    Preparation for release

--- Makefile
+++ Makefile
@@ -54,7 +54,7 @@ run: compile
 	./$(PROGRAM_PATH) 2>&1
 
 debug: compile
-	lldb ./$(PROGRAM_PATH)
+	gdb ./$(PROGRAM_PATH)
 
 doc:
 	doxygen $(DOXYGEN_FILE)
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include "Downloader/Helpers/DownloadFileTree.hpp"
 
 int main(int argc, char *argv[]) {
-  FileDownloader downloader;
-  downloader.download(RemoteReference("https://www.zdravionline.cz/imgs/articles/858-1_vychod_slunce_main.jpg"), LocalReference("test.jpg"));
+ // FileDownloader downloader;
+ // downloader.download(RemoteReference("https://www.zdravionline.cz/imgs/articles/858-1_vychod_slunce_main.jpg"), LocalReference("test.jpg"));
 
   //PageMirror mirror;
   //mirror.mirror(RemoteReference("http://testing-ground.scraping.pro"));
