e644aab218bb01f29ba69ff45dce8415eea5e427
yura_hb <ggaev.work@gmail.com>
2020-06-07 17:52:19 +0200

    Small fixes and improvements

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,5 @@
 #include "Base/ArgumentParser.hpp"
 
 int main(int argc, char *argv[]) {
- // FileDownloader downloader;
- // downloader.download(RemoteReference("https://www.zdravionline.cz/imgs/articles/858-1_vychod_slunce_main.jpg"), LocalReference("test.jpg"));
-
-  //PageMirror mirror;
-  //mirror.mirror(RemoteReference("http://testing-ground.scraping.pro"));
   return ArgumentParser::handle(argc, argv);;
 }
